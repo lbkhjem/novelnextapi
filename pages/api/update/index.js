ac8cc07926b0d53ec01a63ea64468c43c3c07c75
khiêm bá <bakhjem@gmail.com>
2022-11-01 13:56:53 +0700

    add

--- pages/api/update/index.js
+++ pages/api/update/index.js
@@ -1,3 +1,5 @@
+import NextCors from "nextjs-cors";
+
 var express = require("express");
 var router = express.Router();
 const request = require("request-promise");
@@ -5,6 +7,12 @@ const cheerio = require("cheerio");
 var cookieParser = require("cookie-parser");
 
 const handler = async (req, res) => {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
   var novel = [];
   var page = req.query.page;
   const URL =
@@ -33,61 +41,65 @@ const handler = async (req, res) => {
   var update_time = null;
   var id = null;
   var totalpages = null;
-  getPageContent(URL + page).then($ => {
+  getPageContent(URL + page).then(($) => {
     // console.log(
     //   "http://www.nettruyen.com/tim-truyen?status=-1&sort=15&page=" + page
     // );
     // var pagett = $(".pagination-outter ul li.hidden").text();
     // totalpage = pagett.slice(pagett.search("/") + 2);
-    $(".cotgiua .wrap_update .update_item").each(function(result) {
-      
+    $(".cotgiua .wrap_update .update_item").each(function (result) {
       $(this)
         .find("h3.nowrap a")
-        .each(function() {
+        .each(function () {
           novelsname = $(this).text();
-         var novelid = $(this).attr('href');
-         idnovel = novelid.slice(novelid.search(".com/") + 5);
-        //   console.log(idnovel);
+          var novelid = $(this).attr("href");
+          idnovel = novelid.slice(novelid.search(".com/") + 5);
+          //   console.log(idnovel);
         });
       $(this)
         .find(".chapter")
-        .each(function() {
+        .each(function () {
           lasterchapter = $(this).text();
-          var chapterid = $(this).attr('href');
-          idchapter = chapterid.slice(chapterid.search(idnovel+'/') + (idnovel.length+1));
-        //   console.log(idchapter)
+          var chapterid = $(this).attr("href");
+          idchapter = chapterid.slice(
+            chapterid.search(idnovel + "/") + (idnovel.length + 1)
+          );
+          //   console.log(idchapter)
         });
 
-        $(this)
+      $(this)
         .find("a img")
-        .each(function() {
-          cover = $(this).attr('src');
+        .each(function () {
+          cover = $(this).attr("src");
           // console.log(cover);
-        //   console.log(idchapter)
+          //   console.log(idchapter)
         });
-          
-        data.push({
-          'novelsname': novelsname,
-          'idnovel': idnovel,
-          'lasterchapter': lasterchapter,
-          'idchapter': idchapter,
-          'cover': cover
-        })
+
+      data.push({
+        novelsname: novelsname,
+        idnovel: idnovel,
+        lasterchapter: lasterchapter,
+        idchapter: idchapter,
+        cover: cover,
+      });
     });
-    var totalpage = $('.phan-trang a:last-child').text();
-    totalpages = totalpage.slice(5,9);
+    var totalpage = $(".phan-trang a:last-child").text();
+    totalpages = totalpage.slice(5, 9);
     var novels = {
-        url: "https://novelonlinefree.info/novel_list?type=latest&category=all&state=all&page=" + page,
-        page: page,
-        data: data,
-        totalpage: parseInt(totalpages)
-      };
-    
-  try {
-    res.status(200).json(novels);
-  } catch (err) {
-    res.status(500).json({ statusCode: 500, message: err.message });
-  }
-})};
+      url:
+        "https://novelonlinefree.info/novel_list?type=latest&category=all&state=all&page=" +
+        page,
+      page: page,
+      data: data,
+      totalpage: parseInt(totalpages),
+    };
+
+    try {
+      res.status(200).json(novels);
+    } catch (err) {
+      res.status(500).json({ statusCode: 500, message: err.message });
+    }
+  });
+};
 
 export default handler;
--- server.js
+++ server.js
@@ -7,34 +7,35 @@ const port = parseInt(process.env.PORT, 10) || 8002;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
-
+const server = express();
+server.use(cors());
 app.prepare().then(() => {
-  const server = express();
-  var corsOptions = function(req, res, next){ 
-    res.header('Access-Control-Allow-Origin', '*'); 
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 
-    'Content-Type, Authorization, Content-Length, X-Requested-With');
-     next();
-  }
   
-  server.use(corsOptions);
-  server.use(cors());
-  const limiter = rateLimit({
-    windowMs: 10 * 1000, // 15 minutes
-    max: 3, // limit each IP to 100 requests per windowMs
-  });
+  // var corsOptions = function(req, res, next){ 
+  //   res.header('Access-Control-Allow-Origin', '*'); 
+  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  //   res.header('Access-Control-Allow-Headers', 
+  //   'Content-Type, Authorization, Content-Length, X-Requested-With');
+  //    next();
+  // }
+  
+  // server.use(corsOptions);
+  
+  // const limiter = rateLimit({
+  //   windowMs: 10 * 1000, // 15 minutes
+  //   max: 3, // limit each IP to 100 requests per windowMs
+  // });
 
-  server.use(limiter);
-  server.set("trust proxy", 1);
+  // server.use(limiter);
+  // server.set("trust proxy", 1);
 
   server.get("/api/update", (req, res) => {
-    console.log(req.headers);
+    // console.log(req.headers);
     return handle(req, res);
   });
 
   server.get("/api/greet", (req, res) => {
-    console.log(req.headers);
+    // console.log(req.headers);
     return handle(req, res);
   });
   server.get("*/*", (req, res) => {
